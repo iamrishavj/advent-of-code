362c6b30dd22b690885ab29bd0d2e2b699e44e11
iamrishavj <jrishav101@gmail.com>
2024-10-18 22:40:25 +0530

    Completed day 1

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -1,10 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	f, err := os.Open("input.txt")
 
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println("Error while closing the file", err)
+		}
+	}()
+
+	if err != nil {
+		fmt.Println("Could not open input.txt file", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+	temp := 0
+	var maxHeap = [3]int{0, 0, 0}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			addToMaxHeap(temp, &maxHeap)
+			temp = 0
+			continue
+		}
+
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+
+		temp += i
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error while scanning the file")
+		return
+	}
+	finalSum := 0
+	for _, ele := range maxHeap {
+		finalSum += ele
+	}
+
+	fmt.Println("Done!! Answer is", maxHeap)
+	fmt.Println("Added answer", finalSum)
+}
+
+func addToMaxHeap(t int, heap *[3]int) {
+	for i, ele := range heap {
+		if t > ele {
+			// Shift the elements downwards to make room for t
+			copy(heap[i+1:], heap[i:])
+			heap[i] = t
+			break
+		}
+	}
 }
